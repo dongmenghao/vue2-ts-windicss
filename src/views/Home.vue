619f5d82136536bc9534d93990b1bb17d0cf68f6
dongmenghao <dongmh105@qq.com>
2022-07-27 23:19:34 +0800

    upgrade vue2.7

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,33 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>{{ msg }}</div>
+    <div>{{ count?.name }} - {{ count.age }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent, ref } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+// export default Vue.extend({
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
 
-export default Vue.extend({
+// })
+export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld
+  },
+  setup() {
+    const msg = ref('Hello World!')
+    let count = ref({ name: 'lll', age: 18 })
+    return {
+      msg,
+      count,
+    }
   }
 })
 </script>
