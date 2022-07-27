e5b745611ec6986dd9526db82b348a890ce539f1
dongmenghao <dongmh105@qq.com>
2022-07-27 23:20:02 +0800

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,24 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off',
+    'comma-dangle': [
+      // 当最后一个元素或属性与闭括号 ] 或 } 在 不同的行时，允许（但不要求）使用拖尾逗号；当在 同一行时，禁止使用拖尾逗号。
+      'error', {
+        'arrays': 'only-multiline',
+        'objects': 'only-multiline'
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        'singleline': 5,
+        'multiline': {
+          'max': 1,
+          'allowFirstLine': false
+        }
+      }
+    ],
   }
 }
